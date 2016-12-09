44a8f19175171460a1f84085124d43d78ea8e42f
Alex Hudson <ahudson@MacBook-AHudson-2.local>
2016-12-08 20:45:44 -0500

    first commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 2;
+    int pricePerCup = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        displayQuantity(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+    public void submitOrder(View view) {
+        createOrderSummary(calculatePrice());
+    }
+
+    private void displayQuantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }
+
+    private int calculatePrice() {
+        return quantity * pricePerCup;
+    }
+
+    private void createOrderSummary(int price) {
+        String priceMessage = "Name: Alex \nQuantity: " + quantity + " \nTotal: $" + price + "\nThank You!";
+        displayMessage(priceMessage);
+    }
+}
